b365927208e80dad24b5f917634c3681c13d54d6
Rudie Dirkx <github@hotblocks.nl>
2024-04-16 19:43:39 +0200

    larastan + migration

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function index()
+    {
+        $user = User::find(123);
+
+        var_dump($user->email); // exists
+        var_dump($user->name); // old
+        var_dump($user->fullname); // new
+    }
 }
